688c3089086b96a361df78b69b7ea39bc02c42ce
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-19 14:35:55 +0200

    Update exercise

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php 
     $paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio dolores ratione vitae, voluptatem assumenda. Blanditiis, id vel. Earum minima qui accusantium architecto. Adipisci explicabo repudiandae distinctio, nam nihil laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus quis adipisci voluptatibus maxime ratione neque alias nostrum commodi ducimus, illum quae debitis ut ea doloremque molestias suscipit iusto praesentium!'; 
 
+
+    echo str_replace('sit', '***', $paragraph);
+
+/* 
+    !!!!!!!!!!!!!!Così mi da errore,perché ??????
+
     $word = $_GET['word'];
 
-    echo str_replace($word, '***', $paragraph);
+    echo str_replace($word, '***', $paragraph); */
+
+
 ?>
