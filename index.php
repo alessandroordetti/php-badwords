07393ae1229df2d3685c2ad9bd6246125c476332
alessandroordetti <alessandro.ordetti@gmail.com>
2022-04-19 16:49:10 +0200

    Exercise correction

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php 
-    $paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio dolores ratione vitae, voluptatem assumenda. Blanditiis, id vel. Earum minima qui accusantium architecto. Adipisci explicabo repudiandae distinctio, nam nihil laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus quis adipisci voluptatibus maxime ratione neque alias nostrum commodi ducimus, illum quae debitis ut ea doloremque molestias suscipit iusto praesentium!'; 
+    $paragraph = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil odio dolores ratione vitae, voluptatem assumenda. Blanditiis, id vel. Earum minima qui accusantium architecto. Adipisci explicabo repudiandae distinctio, nam nihil laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis delectus quis adipisci voluptatibus maxime ratione neque alias nostrum commodi ducimus, illum quae debitis ut ea doloremque molestias suscipit iusto praesentium! <br>';
+
+    $word = $_GET['word'];
+
+    $message = 'La lunghezza della stringa è: ';
 
 
-    echo str_replace('sit', '***', $paragraph);
+    echo str_replace($word, '***', $paragraph);
+    echo $message . strlen($paragraph);
 
 /* 
     !!!!!!!!!!!!!!Così mi da errore,perché ??????
